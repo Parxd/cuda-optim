bfdd893b0f359f91b8128b999150400c3c8f0d4d
Parxed <daniel.j.park@vanderbilt.edu>
2025-01-21 16:40:50 -0600

    finish kernel 4

--- src/gemm/kernel/4_twodim_blocktile_vectorized.cuh
+++ src/gemm/kernel/4_twodim_blocktile_vectorized.cuh
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include "../../utils.cuh"
 
+// warning: contains a lot of hard-coding, e.g. only using 128-byte transactions
 template <int block_M, int block_N, int block_K, int thread_M, int thread_N>
 __global__ void twodim_blocktile_vectorized(int M, int N, int K, float* A, float* B, float* C) {
     constexpr int threadblock_size = block_M * block_N / (thread_M * thread_N);
@@ -18,8 +19,8 @@ __global__ void twodim_blocktile_vectorized(int M, int N, int K, float* A, float
     constexpr int B_load_tiles = (block_K * block_N / 4) / threadblock_size;
     
     // how many threads needed to cover columns?
-    constexpr int A_load_cols = block_K / 4;  // 2 for default params
-    constexpr int B_load_cols = block_N / 4;  // 16 for default params
+    constexpr int A_load_cols = CEIL_DIV(block_K, 4);  // 2 for default params
+    constexpr int B_load_cols = CEIL_DIV(block_N, 4);  // 16 for default params
     // how many rows can entire threadblock cover in one load_tile iteration?
     constexpr int A_load_rows = threadblock_size / A_load_cols;  // 32 for default params
     constexpr int B_load_rows = threadblock_size / B_load_cols;  // 4 for default params
@@ -36,10 +37,10 @@ __global__ void twodim_blocktile_vectorized(int M, int N, int K, float* A, float
             float4 vector_load = reinterpret_cast<float4*>(
                 &A[(blockIdx.y * block_M + (load_tile * A_load_rows + A_load_y)) * K + (tile * block_K + (A_load_x * 4))]
             )[0];
-            A_tile[load_tile * block_M + linear_idx] = vector_load.x;
-            A_tile[(load_tile + 1) * block_M + linear_idx] = vector_load.y;
-            A_tile[(load_tile + 2) * block_M + linear_idx] = vector_load.z;
-            A_tile[(load_tile + 3) * block_M + linear_idx] = vector_load.w;
+            A_tile[(A_load_x * 4) * block_M + (A_load_rows * load_tile + A_load_y)] = vector_load.x;
+            A_tile[(A_load_x * 4 + 1) * block_M + (A_load_rows * load_tile + A_load_y)] = vector_load.y;
+            A_tile[(A_load_x * 4 + 2) * block_M + (A_load_rows * load_tile + A_load_y)] = vector_load.z;
+            A_tile[(A_load_x * 4 + 3) * block_M + (A_load_rows * load_tile + A_load_y)] = vector_load.w;
         }
         for (int load_tile = 0; load_tile < B_load_tiles; ++load_tile) {
             reinterpret_cast<float4*>(&B_tile[(load_tile * B_load_rows + B_load_y) * block_N + (B_load_x * 4)])[0] = reinterpret_cast<float4*>(
@@ -47,7 +48,26 @@ __global__ void twodim_blocktile_vectorized(int M, int N, int K, float* A, float
             )[0];
         }
         __syncthreads();
-        
+        for (int k = 0; k < block_K; ++k) {
+            for (int m = 0; m < thread_M; ++m) {
+                A_register[m] = A_tile[k * block_M + (threadIdx.y * thread_M + m)];
+            }
+            for (int n = 0; n < thread_N; ++n) {
+                B_register[n] = B_tile[k * block_N + (thread_N * threadIdx.x + n)];
+            }
+            for (int m = 0; m < thread_M; ++m) {
+                for (int n = 0; n < thread_N; ++n) {
+                    res[m * thread_M + n] += A_register[m] * B_register[n];
+                }
+            }
+        }
+        __syncthreads();
+    }
+    for (int m = 0; m < thread_M; ++m) {
+        for (int n = 0; n < thread_N / 4; ++n) {
+            reinterpret_cast<float4*>(&C[(global_idy * thread_M + m) * N + (global_idx * thread_N + (n * 4))])[0] = 
+                reinterpret_cast<float4*>(&res[m * thread_N + (n * 4)])[0];
+        }
     }
 }
 
