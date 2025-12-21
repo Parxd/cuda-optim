52366a269c9678c7276d6ee43a10f64284fa3d1e
Parxed <daniel.j.park@vanderbilt.edu>
2025-12-20 23:35:16 -0500

    complete smem swizzle kernel

--- csrc/transpose/cutlass/smem_swizzle.cu
+++ csrc/transpose/cutlass/smem_swizzle.cu
@@ -1,1 +1,43 @@
+#include <cute/tensor.hpp>
 
+template <class mATensor, class mBTensor, class sALayout, class sBLayout>
+__global__ void __launch_bounds__(256, 1)
+smem_swizzle_transpose(mATensor mA, mBTensor mB, sALayout sA_layout, sBLayout sB_layout) {
+    using namespace cute;
+    using thrLt = Layout<Shape<_8,_32>,Stride<_32,_1>>;
+
+    __shared__ float smem_buffer[cosize_v<sALayout>];
+    auto gA = mA(make_coord(_,_), blockIdx.y, blockIdx.x);
+    auto gB = mB(make_coord(_,_), blockIdx.x, blockIdx.y);
+    auto sA = make_tensor(make_smem_ptr(smem_buffer), sA_layout);
+    auto sB = make_tensor(make_smem_ptr(smem_buffer), sB_layout);
+
+    auto thr_layout = thrLt{};
+    auto tAgA = local_partition(gA, thr_layout, threadIdx.x);
+    auto tAsA = local_partition(sA, thr_layout, threadIdx.x);
+    auto tBsB = local_partition(sB, thr_layout, threadIdx.x);
+    auto tBgB = local_partition(gB, thr_layout, threadIdx.x);
+
+    copy(tAgA, tAsA);
+    __syncthreads();
+    copy(tBsB, tBgB);
+}
+
+void launch_smem_swizzle_transpose(int m, int n, float* A, float* B) {
+    using namespace cute;
+    using thr_lt = Layout<Shape<_8,_32>,Stride<_32,_1>>;
+    
+    auto mA = make_tensor(make_gmem_ptr(A), make_shape(m, n), make_stride(n, _1{}));
+    auto mB = make_tensor(make_gmem_ptr(B), make_shape(m, n), make_stride(n, _1{}));
+    auto tile_shape = make_shape(_64{}, _64{});
+    auto swizzle = Swizzle<5,0,6>{};
+    auto sA_layout = composition(swizzle, make_layout(tile_shape, LayoutRight{}));
+    auto sB_layout = composition(sA_layout, make_layout(tile_shape, LayoutRight{}));
+
+    auto tensor_S = tiled_divide(mA, tile_shape);
+    auto tensor_D = tiled_divide(mB, tile_shape);
+
+    dim3 gridDim(size<1>(tensor_S), size<2>(tensor_S));
+    dim3 blockDim(size(thr_lt{}));
+    smem_swizzle_transpose<<<gridDim, blockDim, 0, nullptr>>>(tensor_S, tensor_D, sA_layout, sB_layout);
+}
--- csrc/transpose/transpose.cpp
+++ csrc/transpose/transpose.cpp
@@ -1,19 +1,31 @@
 #include <cstdio>
 #include <cstdlib>
 #include <assert.h>
-#include <numeric>
 #include <cute/tensor.hpp>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include "../utils.h"
 
 #include "cutlass/smem.cu"
+#include "cutlass/smem_swizzle.cu"
+
+namespace transpose {
+    void run_kernel(int kernel, int m, int n, float* src, float* dst) {
+        if (!kernel) {
+            launch_smem_transpose(m, n, src, dst);
+        }
+        else {
+            launch_smem_swizzle_transpose(m, n, src, dst);
+        }
+    }
+} // namespace transpose
 
 int main(int argc, char* argv[]) {
     int kernel = 0;
     int M = 256;
     int N = 256;
     bool time = false;
+    int trials = 50;
     bool check = false;
 
     if (argc > 1) kernel = std::atoi(argv[1]);
@@ -36,21 +48,38 @@ int main(int argc, char* argv[]) {
 
     auto hA = thrust::host_vector<float>(M * N);
     auto hB = thrust::host_vector<float>(M * N);
-    // std::iota(hA.begin(), hA.end(), 0.0f);
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-    for (int i = 0; i < M * N; ++i) {
-        hA[i] = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
-    }
+    std::iota(hA.begin(), hA.end(), 0.0f);
+    // std::srand(static_cast<unsigned>(std::time(nullptr)));
+    // for (int i = 0; i < M * N; ++i) {
+        // hA[i] = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+    // }
     auto dA = thrust::device_vector<float>(M * N);
     auto dB = thrust::device_vector<float>(M * N);
     dA = hA;
 
-    if (!kernel) {
-        launch_smem_transpose(M, N, dA.data().get(), dB.data().get());
-    }
-    else {
+    if (time) {
+        cudaEvent_t start, stop;
+        CUDA_CHECK(cudaEventCreate(&start));
+        CUDA_CHECK(cudaEventCreate(&stop));
+        transpose::run_kernel(kernel, M, N, dA.data().get(), dB.data().get());
 
+        CUDA_CHECK(cudaEventRecord(start));
+        for (int i = 0; i < trials; ++i) {
+            transpose::run_kernel(kernel, M, N, dA.data().get(), dB.data().get());
+        }
+        CUDA_CHECK(cudaEventRecord(stop));
+        CUDA_CHECK(cudaEventSynchronize(stop));
+        float milliseconds = 0;
+        CUDA_CHECK(cudaEventElapsedTime(&milliseconds, start, stop));
+        float avg_time = milliseconds / trials;
+
+        std::cout << "[Transpose]: Avg. walltime (" << trials << " trials): " << avg_time << " ms\n";       
+
+        CUDA_CHECK(cudaEventDestroy(start));
+        CUDA_CHECK(cudaEventDestroy(stop));
     }
+
+    transpose::run_kernel(kernel, M, N, dA.data().get(), dB.data().get());
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
     hB = dB;
