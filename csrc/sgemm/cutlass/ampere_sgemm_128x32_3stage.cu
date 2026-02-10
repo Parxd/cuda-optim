8b2b9c1fcb643b72df325d8a28a48e8d9e3a5dcc
Parxed <daniel.j.park@vanderbilt.edu>
2026-02-10 18:14:47 -0500

    rename + add kernel hotloop

--- csrc/sgemm/cutlass/ampere_sgemm_128x32_3stage.cu
+++ csrc/sgemm/cutlass/ampere_sgemm_128x32_3stage.cu
@@ -2,12 +2,12 @@
 #include "cute/layout.hpp"
 #include <cute/tensor.hpp>
 
-namespace ampere_sgemm_128x32_db_pipe {
+namespace ampere_sgemm_128x32_3stage {
 
 template <typename strideA, typename strideB, typename strideC,
           typename sALayout, typename sBLayout,
           typename ctaShape, typename copyPolicyA, typename copyPolicyB, typename mmaPolicy>
-__global__ void ampere_sgemm_128x32_db_pipe(
+__global__ void ampere_sgemm_128x32_3stage(
     int m, int n, int k, float alpha, float beta,
     const float* __restrict__ A, strideA stride_A,
     const float* __restrict__ B, strideB stride_B,
@@ -38,23 +38,81 @@ __global__ void ampere_sgemm_128x32_db_pipe(
     auto tBgB = tB.partition_S(gB);
     auto tBsB = tB.partition_D(sB);
 
+    uint gmem_tile_idx = 0;
     uint gmem_tiles = size<2>(gA);
-    uint smem_pipes = size<2>(sA);
-    uint rmem_blocks = size<2>(tAsA);
+    constexpr uint smem_pipes = size<2>(sA);
     
     // prefetch for first (smem_pipes - 1) pipes
     for (uint i = 0; i < smem_pipes - 1; ++i) {
-        copy(copy_A, tAgA(_,_,_,i), tAsA(_,_,_,i));
-        copy(copy_B, tBgB(_,_,_,i), tBsB(_,_,_,i));
+        copy(copy_A, tAgA(_,_,_,gmem_tile_idx), tAsA(_,_,_,i));
+        copy(copy_B, tBgB(_,_,_,gmem_tile_idx), tBsB(_,_,_,i));
+        cp_async_fence();
         --gmem_tiles;
+        if (gmem_tiles) {
+            ++gmem_tile_idx;
+        }
     }
-    cp_async_fence();
-    
-    // if (thread0()) {
-        // print(sA); print("\n");
-        // print(tAgA.layout()); print("\n");
-        // print(tAsA.layout()); print("\n");
-    // }
+
+    auto tC = tiled_mma.get_thread_slice(threadIdx.x);
+    auto tCsA = tC.partition_A(sA);
+    auto tCsB = tC.partition_B(sB);
+    auto tCgC = tC.partition_C(gC);
+    auto tCrA = make_fragment_like(tCsA(_,_,_,0));
+    auto tCrB = make_fragment_like(tCsB(_,_,_,0));
+    auto tCrC = make_fragment_like(tCgC);
+    fill(tCrC, 0.0);
+
+#if 0
+    if (thread0()) {
+        print(tAgA); print("\n");
+        print(tBgB); print("\n");
+
+        print(tAsA); print("\n");
+        print(tBsB); print("\n");
+
+        print(tCrA); print("\n");
+        print(tCrB); print("\n");
+    }
+#endif
+
+    cp_async_wait<smem_pipes - 2>();
+    __syncthreads();
+    // prefetch r_block = 0
+    copy(tCsA(_,_,0,0), tCrA(_,_,0));  // (M, K, pipe)
+    copy(tCsB(_,_,0,0), tCrB(_,_,0));
+
+    uint pipe_read = 0;
+    uint pipe_write = smem_pipes - 1;
+    constexpr uint rmem_blocks = size<2>(tCrA);
+    const uint k_iters = gmem_tiles + (smem_pipes - 1);
+
+    for (uint iter = 0;  iter < k_iters; ++iter) {
+        if (iter < gmem_tiles) {
+            copy(copy_A, tAgA(_,_,_,gmem_tile_idx), tAsA(_,_,_,pipe_write));    
+            copy(copy_B, tBgB(_,_,_,gmem_tile_idx), tBsB(_,_,_,pipe_write));
+        }
+        cp_async_fence();
+        cp_async_wait<smem_pipes - 2>();
+        __syncthreads();
+
+        CUTE_UNROLL
+        for (uint block = 0; block < rmem_blocks - 1; ++block) {
+            gemm(tiled_mma, tCrA(_,_,block), tCrB(_,_,block), tCrC);
+            copy(tCsA(_,_,block+1,pipe_read), tCrA(_,_,block+1));
+            copy(tCsB(_,_,block+1,pipe_read), tCrB(_,_,block+1));
+        }
+        gemm(tiled_mma, tCrA(_,_,rmem_blocks-1), tCrB(_,_,rmem_blocks-1), tCrC);
+        
+        pipe_write = pipe_read;
+        pipe_read = (pipe_read + 1) % smem_pipes;
+
+        if (iter != k_iters - 1) {
+            copy(tCsA(_,_,0,pipe_read), tCrA(_,_,0));
+            copy(tCsB(_,_,0,pipe_read), tCrB(_,_,0));
+        }
+        ++gmem_tile_idx;
+    }
+    axpby(alpha, tCrC, beta, tCgC);
 }
 
 void nn(int m, int n, int k, float alpha,
@@ -91,11 +149,10 @@ void nn(int m, int n, int k, float alpha,
     );
     auto mma = make_tiled_mma(
         MMA_Atom<UniversalFMA<float>>{},
-        make_layout(make_shape(Int<16>{}, Int<16>{})),
-        Tile<_128, _128, decltype(select<2>(cta_shape))>{}
+        make_layout(make_shape(Int<16>{}, Int<16>{}))
     );
 
-    auto kernel = ampere_sgemm_128x32_db_pipe<decltype(stride_A), decltype(stride_B), decltype(stride_C),
+    auto kernel = ampere_sgemm_128x32_3stage<decltype(stride_A), decltype(stride_B), decltype(stride_C),
                                               decltype(sA_layout), decltype(sB_layout), decltype(cta_shape),
                                               decltype(copy_A), decltype(copy_B), decltype(mma)>;
     cudaFuncSetAttribute(kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
@@ -108,15 +165,15 @@ void nn(int m, int n, int k, float alpha,
     );
 }
 
-}  // namespace ampere_sgemm_128x32_db_pipe
+}  // namespace ampere_sgemm_128x32_3stage
 
-void launch_ampere_sgemm_128x32_db_pipe(
+void launch_ampere_sgemm_128x32_3stage(
     char transA, char transB, int m, int n, int k,
     float alpha, const float* A, int ldA,
     const float* B, int ldB, float beta,
     float* C, int ldC, cudaStream_t stream = 0
 ) {
     if (transA == 'N' && transB == 'N') {
-        ampere_sgemm_128x32_db_pipe::nn(m, n, k, alpha, A, ldA, B, ldB, beta, C, ldC);
+        ampere_sgemm_128x32_3stage::nn(m, n, k, alpha, A, ldA, B, ldB, beta, C, ldC);
     }
 }
